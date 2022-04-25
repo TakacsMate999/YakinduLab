713b4f377dbb960a124b571ff88fc5608ec2941d
taki <takacs.mate999@gmail.com>
2022-04-25 15:43:39 +0200

    3. feladat progress

--- hu.bme.mit.yakindu.analysis/src/hu/bme/mit/yakindu/analysis/workhere/RunStatechart.java
+++ hu.bme.mit.yakindu.analysis/src/hu/bme/mit/yakindu/analysis/workhere/RunStatechart.java
@@ -1,9 +1,11 @@
 package hu.bme.mit.yakindu.analysis.workhere;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 
-// import hu.bme.mit.yakindu.analysis.RuntimeService;
-// import hu.bme.mit.yakindu.analysis.TimerService;
+import hu.bme.mit.yakindu.analysis.RuntimeService;
+import hu.bme.mit.yakindu.analysis.TimerService;
 import hu.bme.mit.yakindu.analysis.example.ExampleStatemachine;
 import hu.bme.mit.yakindu.analysis.example.IExampleStatemachine;
 
@@ -12,20 +14,30 @@ import hu.bme.mit.yakindu.analysis.example.IExampleStatemachine;
 public class RunStatechart {
 	
 	public static void main(String[] args) throws IOException {
-//		ExampleStatemachine s = new ExampleStatemachine();
-//		s.setTimer(new TimerService());
-//		RuntimeService.getInstance().registerStatemachine(s, 200);
-//		s.init();
-//		s.enter();
-//		s.runCycle();
-//		print(s);
-//		s.raiseStart();
-//		s.runCycle();
-//		System.in.read();
-//		s.raiseWhite();
-//		s.runCycle();
-//		print(s);
-//		System.exit(0);
+		ExampleStatemachine s = new ExampleStatemachine();
+		s.setTimer(new TimerService());
+		RuntimeService.getInstance().registerStatemachine(s, 200);
+		s.init();
+		s.enter();
+		s.runCycle();
+		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+	    String line;
+		while((line = bf.readLine()) != null && !line.equalsIgnoreCase("exit")) {
+			if(line.equals("start")) {
+				s.raiseStart();
+				s.runCycle();
+				print(s);
+			}else if(line.equals("white")) {
+				s.raiseWhite();
+				s.runCycle();
+				print(s);
+			}else if (line.equals("black")) {
+				s.raiseBlack();
+				s.runCycle();
+				print(s);
+			}
+		}
+		System.exit(0);
 	}
 
 	public static void print(IExampleStatemachine s) {
